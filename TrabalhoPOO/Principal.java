d4a0c92194493650257eb57f465d268d3d966696
Danilo Moreira Ribeiro <danilomoreiraribeiro@hotmail.com>
2026-04-15 13:13:23 -0300

    Initialize main method and add Data and Aluno arrays

--- TrabalhoPOO/Principal.java
+++ TrabalhoPOO/Principal.java
@@ -1,5 +1,10 @@
 public class Principal {
    public static void main(String args[]) {
+      Data  nascimento[] = new Data[3];
+      Aluno alunos[] = new Aluno[3];
       
+      nascimento[0] = new Data(24, 05, 2004);
+      aluno[0] = new Aluno("Danilo Moreira Ribeiro", "12345678900", nascimento[0], "42991647496", "25031526");
+
    }
 }
