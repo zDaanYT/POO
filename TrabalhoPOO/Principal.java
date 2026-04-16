cd88b683a8d53eb47c30fb979f2fb79bb48de54c
Danilo Moreira Ribeiro <danilomoreiraribeiro@hotmail.com>
2026-04-16 16:21:54 -0300

    Refactor Principal class and update student data
    Updated the 'nascimento' array initialization and changed 'aluno' to 'alunos'. Added a method to set the birth date for the first student.

--- TrabalhoPOO/Principal.java
+++ TrabalhoPOO/Principal.java
@@ -1,10 +1,14 @@
 public class Principal {
    public static void main(String args[]) {
       Data  nascimento[] = new Data[3];
-      Aluno aluno[] = new Aluno[3];
+      Aluno alunos[] = new Aluno[3];
       
-      nascimento[0] = new Data(24, 05, 2004);
-      aluno[0] = new Aluno("Danilo Moreira Ribeiro", "12345678900", nascimento[0], "42991647496", "25031526");
+      nascimento[0] = new Data(00, 00, 0000);
+      alunos[0] = new Aluno("Danilo Moreira Ribeiro", "12345678900", nascimento[0], "42991647496", "25031526");
+      alunos[0].setNascimento(new Data(24, 05, 2004));
+      
+      System.out.println(aluno[0]);
+      
 
    }
 }
