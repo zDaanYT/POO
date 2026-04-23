6475fa3ce4eb92fa6454fb5e09c6903451b6e89b
Danilo Moreira Ribeiro <danilomoreiraribeiro@hotmail.com>
2026-04-23 08:30:26 -0300

    Atualização vetor aluno.

--- TrabalhoPOO/Principal.java
+++ TrabalhoPOO/Principal.java
@@ -1,14 +1,19 @@
 public class Principal {
    public static void main(String args[]) {
       Data  nascimento[] = new Data[6];
-      Aluno alunos[] = new Aluno[1];
+      Aluno aluno[] = new Aluno[1];
       Funcionario chefe[] = new Chefe[1];
       
       nascimento[0] = new Data(00, 00, 0000);
-      alunos[0] = new Aluno("Danilo Moreira Ribeiro", "12345678900", nascimento[0], "42991647496", "25031526");
-      alunos[0].setNascimento(new Data(24, 05, 2004));
+      aluno[0] = new Aluno();
+      aluno[0].setNome("Danilo Moreira Ribeiro");
+      aluno[0].setCpf("12345678900");
+      aluno[0].setNascimento(nascimento[0]);
+      aluno[0].setTelefone("42991647496");
+      aluno[0].setMatricula("25031526");
+      aluno[0].setNascimento(new Data(24, 05, 2004));
       
-      System.out.println(alunos[0]);
-      // Commit teste
+      System.out.println(aluno[0]);
+      
    }
 }
