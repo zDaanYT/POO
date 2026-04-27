a59c14cf4b6ebce2ee588e4b2a43a37d9a5ffdff
Danilo Moreira Ribeiro <danilomoreiraribeiro@hotmail.com>
2026-04-27 16:32:43 -0300

    Refactor Chefe and Professor initialization
    Updated Chefe and Professor object initialization and output formatting.

--- TrabalhoPOO/Principal.java
+++ TrabalhoPOO/Principal.java
@@ -19,32 +19,16 @@ public class Principal {
       
       if (aluno[0].getMatricula() == true) {
          System.out.println("Aluno matriculado com sucesso!");
-         System.out.println(aluno[0]);
+         System.out.println(aluno[0] + "\n");
       } else { System.out.println("Matricula falhou."); }
 
       // CHEFE
 
-      chefe[0] = new Chefe("Carlos", "00876543256", new Data(12, 1, 1980), "Dimetor", 4000.00, "1556789", "");
-      chefe[0].setNome("Carlos Silva");
-      chefe[0].setCPF("09876543210");
-      chefe[0].setCargo("Diretor");
-      chefe[0].setTelefone("4284000000");
-      chefe[0].setRegistro("1456789");
-      chefe[0].setSalario(5000.00);
-      chefe[0].setNascimento(new Data(15, 8, 1980));
-
-      System.out.println(chefe[0]);
+      chefe[0] = new Chefe("Carlos", "52998224725", new Data(15, 8, 1980), "42991640000", 5000.0, "12456789", "Diretor");
+      System.out.println(chefe[0] + "\n");
 
       professor[0] = new Professor("A", "A", new Data(12, 1, 1980), 0.0, "A", 0);
-      professor[0].setNome("Maria Oliveira");
-      professor[0].setCPF("11223344556");
-      professor[0].setTelefone("42991660000");
-      professor[0].setRegistro("9876543");
-      professor[0].setSalario(3000.00);
-      professor[0].setHorasAula(40);
-      professor[0].setNascimento(new Data(10, 12, 1985));
-
-      System.out.println(professor[0]);
+      System.out.println(professor[0] + "\n");
 
       funcionario[0] = new Funcionario("A", "A", new Data(12, 1, 1980), "A", 0.0, "A");
       funcionario[0].setNome("João Santos");
