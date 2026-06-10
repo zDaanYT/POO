4071a07ed2d141d54f3c70f464b5121c1920d5ab
Danilo Moreira Ribeiro <danilomoreiraribeiro@hotmail.com>
2026-06-10 15:57:55 -0300

    Atualizar o MinhaExcecao.java

--- TrabalhoPOOII/MinhaExcecao.java
+++ TrabalhoPOOII/MinhaExcecao.java
@@ -1,1 +1,1 @@
-public class MinhaExcecao extends Exception { public MinhaExcecao (String erros) {} }
+public class MinhaExcecao extends Exception { public MinhaExcecao (String erros) { super(erros); } }
